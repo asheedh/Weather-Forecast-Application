52f8182db2d851d55d7eb3ffb45db72d6ae76035
Azeez Saginala <saginalaazeez21@gmail.com>
2024-09-16 13:34:35 +0530

    have to complete Responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./wfad.html","./wfad.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-up': 'slide-up 0.9s ease-out',
+      },
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
--- wfad.js
+++ wfad.js
@@ -1,5 +1,3 @@
-console.log('working');
-
 const search = document.querySelector("#search");
 const searchField = document.querySelector("#city");
 const content = document.querySelector("#content");
@@ -7,14 +5,15 @@ const temp = document.querySelector("#temp");
 const humidity = document.querySelector("#humidity");
 const windSpeed = document.querySelector("#wind");
 const location1 = document.querySelector("#location");
-const day1 = document.querySelector("#d1");
-const day2 = document.querySelector("#d2");
-const day3 = document.querySelector("#d3");
-const day4 = document.querySelector("#d4");
-const day5 = document.querySelector("#d5");
-const day6 = document.querySelector("#d6");
-const day7 = document.querySelector("#d7");
-const body = document.querySelector("body");
+let day1 = document.querySelector("#d1");
+let day2 = document.querySelector("#d2");
+let day3 = document.querySelector("#d3");
+let day4 = document.querySelector("#d4");
+let day5 = document.querySelector("#d5");
+let day6 = document.querySelector("#d6");
+let day7 = document.querySelector("#d7");
+const body = document.querySelector("#mainCard");
+const condition = document.querySelector("#condition")
 
 
 
@@ -22,7 +21,6 @@ const APIkey = "e1cfcbdeb8634f3b94862203241309";
 const URL = "https://api.weatherapi.com/v1/forecast.json?";
 
 content.innerHTML = `<div class="loader">Loading...</div>`;
-
 
 fetch("http://ip-api.com/json/")
     .then(res => res.json())
@@ -34,7 +32,6 @@ fetch("http://ip-api.com/json/")
             .then(position => {
                 // Store the data in sessionStorage
                 sessionStorage.setItem(city, JSON.stringify(position));
-                console.log(position)
 
                 // Display the weather data
                 displayWeather(position);
@@ -43,7 +40,7 @@ fetch("http://ip-api.com/json/")
             })
             .catch(error => {
                 console.error('Error:', error);
-                content.innerHTML = "Unable to find Users location";
+                
             });
             
     })
@@ -72,27 +69,28 @@ function displayWeather(data) {
             return 'linear-gradient(to top, #ffffff, #cccccc)'; // Default gradient for unknown conditions
         }
     }
-    body.style.background = getBackgroundColor(data.forecast.forecastday[0].day.condition.text)
+    
     
     content.innerHTML = `<img src="https:${data.forecast.forecastday[0].day.condition.icon}"> City: ${searchField.value}`;
     temp.innerHTML = `<i class="wi wi-thermometer text-3xl"></i>${Math.round(data.current.temp_c)}°C`;
     humidity.innerHTML = `${data.current.humidity} %`;
     windSpeed.innerHTML = `${data.current.wind_kph} Kph`;
     location1.innerHTML = `Latitude: ${data.location.lat} <br> Longitude: ${data.location.lon}`;
+    condition.innerHTML = `${data.forecast.forecastday[0].day.condition.text}`
 
     const days = [day1, day2, day3, day4, day5, day6, day7];
     days.forEach((day, index) => {
         const forecastingOf = data.forecast.forecastday[index + 1];
         day.innerHTML = `<img src="https:${forecastingOf.day.condition.icon}">
-        <br> Forecast Date: ${forecastingOf.date} 
-        <br> Avg temp: ${Math.round(forecastingOf.day.avgtemp_c)} °C 
-        <br> Avg Wind speed: ${forecastingOf.day.maxwind_kph} Kph 
-        <br> Avg Humidity: ${forecastingOf.day.avghumidity} % 
-        <br> Day's Condition: ${forecastingOf.day.condition.text}`;
+        <br> Forecast Date: <strong>${forecastingOf.date}</strong>
+        <br> Avg temp: <strong>${Math.round(forecastingOf.day.avgtemp_c)} °C </strong>
+        <br> Avg Wind speed: <strong>${forecastingOf.day.maxwind_kph} Kph </strong>
+        <br> Avg Humidity: <strong>${forecastingOf.day.avghumidity} % </strong>
+        <br> Day's Condition: <strong>${forecastingOf.day.condition.text} </strong>`;
         day.style.background = getBackgroundColor(forecastingOf.day.condition.text)
+        body.style.background = getBackgroundColor(data.forecast.forecastday[0].day.condition.text)
     })
 
-    console.log(data);
 }
 
 // Event listener for the search button
@@ -108,7 +106,7 @@ search.addEventListener('click', (event) => {
 
     } 
     else if (!/^[a-zA-Z\s]+$/.test(city)) {
-        alert("Please enter a valid city name");
+        alert("City name shouldn't contain any special characters ");
 
     }
     
@@ -133,7 +131,20 @@ search.addEventListener('click', (event) => {
             })
             .catch(error => {
                 console.error('Error:', error);
-                content.innerHTML = "Failed to retrieve weather data.";
-            });
+                content.innerHTML = `Unable to find Users location "${searchField.value}"`;
+                temp.innerHTML = `Not available !!!`;
+                humidity.innerHTML = `Not available !!!`;
+                windSpeed.innerHTML = `Not available !!!`;
+                location1.innerHTML = `Not available !!!`;
+                condition.innerHTML = `Not available !!!`;
+
+                const days = [day1, day2, day3, day4, day5, day6, day7];
+                days.forEach((day, index) => {
+                    day.innerHTML = `Not available !!!`
+                    day.style.background = "linear-gradient(to top, blue, white)"
+                    body.style.background = "linear-gradient(to top, white, blue)"
+                    searchField.innerHTML = '';
+                })
+             });
     }
 });
